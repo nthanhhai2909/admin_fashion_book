f807f1d8ced6fd9124061fddf5a86120a0d51da6
Nguyen Thanh Hai <nthanhhai2909@gmail.com>
2018-05-22 23:30:20 +0700

    add navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomeContainer from './home.container'
-
+import NavbarContainer from './navbar.container'
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={HomeContainer} />
-    </Switch>
-  </Router>
+    <section id="container" class="">
+        <NavbarContainer/>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={HomeContainer} />
+            </Switch>
+        </Router>
+    </section>
+
+
 )
 
 export default App;
---
+++ src/containers/navbar.container.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Navbar from '../components/navbar/navbar'
+class NavbarContainer extends Component {
+    
+    render() {
+        return(
+            <div>
+                <Navbar/>
+            </div>
+        )
+    }
+
+}
+export default NavbarContainer
